7bf72acc397b9474a19f76bed2935a3884208988
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2025-07-06 16:05:06 +0300

    First commit

---
+++ config/web.php
@@ -0,0 +1,139 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'modules' => [
+        'gridview' => [
+            'class' => 'kartik\grid\Module',
+            // uncomment the following to enable the export menu
+            'exportConfig' => [
+                \kartik\grid\GridView::CSV => [
+                    'label' => 'CSV',
+                    'icon' => 'file-text',
+                    'iconOptions' => ['class' => 'text-primary'],
+                    'showHeader' => true,
+                    'showPageSummary' => true,
+                    'showFooter' => true,
+                    'showCaption' => true,
+                    'filename' => 'export',
+                    'alertMsg' => 'The CSV export file will be generated for download.',
+                    'options' => ['title' => 'Comma Separated Values'],
+                ],
+                \kartik\grid\GridView::EXCEL => [
+                    'label' => 'Excel',
+                    'icon' => 'file-excel',
+                    'iconOptions' => ['class' => 'text-success'],
+                    'showHeader' => true,
+                    'showPageSummary' => true,
+                    'showFooter' => true,
+                    'showCaption' => true,
+                    'filename' => 'export',
+                    'alertMsg' => 'The EXCEL export file will be generated for download.',
+                    'options' => ['title' => 'Microsoft Excel 95+'],
+                ],
+                \kartik\grid\GridView::PDF => [
+                    'label' => 'PDF',
+                    'icon' => 'file-pdf',
+                    'iconOptions' => ['class' => 'text-danger'],
+                    'showHeader' => true,
+                    'showPageSummary' => true,
+                    'showFooter' => true,
+                    'showCaption' => true,
+                    'filename' => 'export',
+                    'alertMsg' => 'The PDF export file will be generated for download.',
+                    'options' => ['title' => 'Portable Document Format'],
+                ],
+                \kartik\grid\GridView::TEXT => [
+                    'label' => 'Text',
+                    'icon' => 'file-text',
+                    'iconOptions' => ['class' => 'text-info'],
+                    'showHeader' => true,
+                    'showPageSummary' => true,
+                    'showFooter' => true,
+                    'showCaption' => true,
+                    'filename' => 'export',
+                    'alertMsg' => 'The TEXT export file will be generated for download.',
+                    'options' => ['title' => 'Plain Text Format'],
+                ],
+
+            ],
+        ],
+    ],
+    'components' => [
+        'i18n' => [
+            'translations' => [
+                'yii2-ajaxcrud' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@yii2ajaxcrud/ajaxcrud/messages',
+                    'sourceLanguage' => 'en',
+                ],
+            ]
+        ],
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'GP0uttJUcL3GQ-5Qlruw3yUxCH-nUokP',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@app/mail',
+            // send all mails to a file by default.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [],
+        ],
+
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
