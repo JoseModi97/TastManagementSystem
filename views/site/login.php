f0b2ffbef2b3e69d1fb3f6d778f8c4eb1cd4637d
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2025-07-07 11:24:06 +0300

    Update login.php

--- views/site/login.php
+++ views/site/login.php
@@ -56,12 +56,6 @@ $this->title = 'Login';
             <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-user btn-block', 'name' => 'login-button']) ?>
 
             <hr>
-            <a href="#" class="btn btn-google btn-user btn-block disabled"> {/* Placeholder - implement actual OAuth if needed */}
-                <i class="fab fa-google fa-fw"></i> Login with Google
-            </a>
-            <a href="#" class="btn btn-facebook btn-user btn-block disabled"> {/* Placeholder - implement actual OAuth if needed */}
-                <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-            </a>
             <?php ActiveForm::end(); ?>
             <hr>
             <div class="text-center">
