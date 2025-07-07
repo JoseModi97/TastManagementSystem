8181084efa513dd90ec783d75ea861bd24574e9f
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2025-07-07 19:26:48 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'name' => 'TMIS', // Added application name
+    'name' => 'TSMIS', // Added application name
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
